d963a4a1e04ed4431c2d948e0a514d163cf07e5b
anindya73 <anindyakhanra90@gmail.com>
2026-03-25 18:52:14 +0530

    server creation

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = 3000;
+
+const server = http.createServer();
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
